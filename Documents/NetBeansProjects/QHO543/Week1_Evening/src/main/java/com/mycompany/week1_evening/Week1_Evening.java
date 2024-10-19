951d6710f48d63a7331fb47bd47cb8558f56fb0b
khadijakatanani <127528719+khadijakatanani@users.noreply.github.com>
2024-10-19 12:51:58 +0100

    week 1

---
+++ Documents/NetBeansProjects/QHO543/Week1_Evening/src/main/java/com/mycompany/week1_evening/Week1_Evening.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.week1_evening;
+// hhhhhhh
+import java.util.Scanner;
+
+/**
+ *
+ * @author weekend Khadija
+ */
+public class Week1_Evening {
+
+    String loginName, password;
+    String age;
+    boolean isSaved;
+
+    public static void main(String[] args) {
+        
+        Week1_Evening week1 = new Week1_Evening();        
+        week1.scannerExercise();      
+
+    }
+
+    private void scannerExercise() {
+    
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Type your name: ");
+        loginName = scanner.next();
+
+        System.out.println("Type your password: ");
+        password = scanner.next();
+
+        System.out.println("Type your age: ");
+        age = scanner.next();
+
+        float castAge = Float.parseFloat(age);
+
+        System.out.println("Student name " + loginName + " with age " + castAge + "!");
+
+        if (loginName.equals("Khadija") && password.equals("Solent")) {
+            System.out.println("Sign in successful!");
+        } else {
+            System.out.println("Sign in failed!");
+        }
+
+        int i = 1;
+
+        while (i < 11) {
+            System.out.println("Square of number " + i + " is " + i * i);
+            i++;
+        }
+    
+    
+    
+    }
+}
