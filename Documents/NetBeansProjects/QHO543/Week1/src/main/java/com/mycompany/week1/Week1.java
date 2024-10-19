97a63ba20c1bc4de85503e2bfdfa91a66e00df17
khadijakatanani <127528719+khadijakatanani@users.noreply.github.com>
2024-10-19 12:23:58 +0100

    week 1

---
+++ Documents/NetBeansProjects/QHO543/Week1/src/main/java/com/mycompany/week1/Week1.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.week1;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Khadija
+ */
+public class Week1 {
+
+    public static void main(String[] args) {          
+        
+        Scanner scanner = new Scanner(System.in);  
+        System.out.print("Please type a value between 1 and 25 inclusive:");
+        int number = scanner.nextInt(); 
+        
+        while (number<0 || number>25 ){
+            System.out.print("Please type a value between 1 and 25 inclusive:");
+            number = scanner.nextInt();            
+        }
+       
+        if (number % 2 == 0){
+            System.out.println("The number is even!");
+        } else {
+            System.out.println("The number is odd!");
+        }
+        
+        
+        if (number > 0){
+            System.out.println("The number is positive");           
+        } else if (number < 0){
+            System.out.println("The number is negative");
+            
+        } else {
+            System.out.println("The number is zero");
+        } 
+    }
+}
